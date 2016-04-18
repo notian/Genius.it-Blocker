d7fd31b7331f2ffa9d1a414363889778397c9b6e
Ian Prentice <ian@d2cmedia.ca>
2016-04-18 11:40:15 -0400

    Initial commit

---
+++ genius.it.block.js
@@ -0,0 +1,11 @@
+/* No Config genius.it blocker */
+(function(){
+	var l = document.location;
+	if( l.host == 'genius.it' ){
+		var p = l.pathname.substring(1).split('/');
+		if( parseInt( p[0] ).toString() == p[0]  ) {
+			p.shift(); /* Get rid Genius ID (if applicable) */
+		}
+		document.location = '//'+p.join('/');
+	}
+})();
